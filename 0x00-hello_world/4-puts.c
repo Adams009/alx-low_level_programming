10a353f2b8e71c6dfa7edf7259cee7738841d279
adams009 <olaadam003@gmail.com>
2023-09-08 04:00:02 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point for program
  *
- * return: Always return "0" (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdioio.h>
+/**
+ * main - this like a room where your program starts
+ *
+ * Return : Always return 0 (suceess)
+ * /
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
