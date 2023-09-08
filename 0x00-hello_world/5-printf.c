453f198474af58852f8f2534c70e024b5d1d0c46
adams009 <olaadam003@gmail.com>
2023-09-08 04:08:01 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
  * main - this like a room where your program starts
  *
  * Return : Always return 0 (suceess)
- * /
+ */
 int main (void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n")
