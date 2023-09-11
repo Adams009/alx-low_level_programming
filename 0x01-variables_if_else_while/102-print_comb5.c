d19bbf4aca723d3d2d7d6a63117d7116c16381ac
adams009 <olaadam003@gmail.com>
2023-09-11 16:00:27 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - where yout program starts
+ *
+ * Return: Always '0' for (success)
+ */
+int main(void)
+{
+	int a, z;
+
+	for (a = 0; a <= 98; a++)
+	for (z = a + 1; z <= 99; z++)
+	{
+		putchar((a / 10) + '0');
+		putchar((a % 10) + '0');
+		putchar(' ');
+		putchar((z / 10) + '0');
+		putchar((z % 10) + '0');
+
+	if (a == 98 && z == 99)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		putchar(',');
+		putchar(' ');
+	
+	}
+	}
+	return (0);
+}
