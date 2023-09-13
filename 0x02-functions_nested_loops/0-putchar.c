94626d6d5d293b477cdee4dd455a129f2bce0243
adams009 <olaadam003@gmail.com>
2023-09-13 14:07:03 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - check the code and the starting point
+ *
+ * Return: Always return 0 (success)
+ */
+int main(void)
+{
+	int p;
+	char c[] = "_putchar";
+
+	for (p = 0; p <= 7; p++)
+{
+	_putchar(c[p]);
+}
+	_putchar('\n');
+	return (0);
+}
