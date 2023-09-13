163d396f3048d1262ce3649f3940d104bab04393
adams009 <olaadam003@gmail.com>
2023-09-13 16:44:11 -0700

    go

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,9 @@
 /**
  * _islower - integer for lowercase
  *
- * @c - it will have be ASCII number for c
+ * Description: to know lowercase
+ *
+ *@c: it will have be ASCII number for c
  *
  * Return: (1) if c is lower otherwise (0)
  */
@@ -11,10 +13,10 @@ int _islower(int c)
 	char l = c;
 	int clower = 0;
 
-	if (l > 'a' && l <='z')
+	if (l > 'a' && l <= 'z')
 	{
 		clower = 1;
 	}
 
-		return(clower);
+		return (clower);
 }
