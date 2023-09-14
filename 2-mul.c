46b01a37dc96970f4f1921ee398d49362bcd2745
adams009 <olaadam003@gmail.com>
2023-09-14 14:15:51 -0700

    all 3

---
+++ 2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * mul(int a, int b) - function that multiplies two integers.
+ *
+ * @a: the int number
+ *
+ * @b: the sec number
+ *
+ * Return: the result of a and b
+ */
+int mul(int a, int b)
+
+{
+	int rlt = (a * b);
+	
+	return (rlt);
+}
