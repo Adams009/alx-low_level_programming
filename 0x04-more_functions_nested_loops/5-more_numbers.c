19115beae25eed4f0e85f21e102d16cd49716516
adams009 <olaadam003@gmail.com>
2023-09-14 18:17:58 -0700

    yo

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,23 +2,21 @@
 /**
  * more_numbers - function that prints 10 times the numbers, from 0 to 14
  *
- * void
+ *
  */
 void more_numbers(void)
 {
-	int y,z;
+	int y, z;
 
-	for
-		(y = 1; y <= 10; y++)
-		{
-			for
-				(z = 0; z <= 14; z++)
-				{
+	for (y = 0; y < 10; y++)
+	{
+			for (z = 0; z < 15; z++)
+			{
 					if
 						(z >= 10)
-			_putchar('y');
-			_putchar(z % 10 8 '0');
-				}
+			_putchar(z / 10 * '0');
+			_putchar(z % 10 * '0');
+			}
 			_putchar('\n');
-		}
+	}
 }
