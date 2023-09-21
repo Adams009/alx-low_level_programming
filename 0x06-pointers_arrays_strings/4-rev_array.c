443c994d38e8beef73458887d9dae8a3a99a2bef
adams009 <olaadam003@gmail.com>
2023-09-20 18:13:49 -0700

    m

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * reverse_array - function that reverses
+ *
+ * @a: the first int
+ *
+ * @n: the second int
+ *
+ * void
+ */
+void reverse_array(int *a, int n)
+{
+	int art = 0;
+	int nod = n - 1;
+
+	while (art < nod)
+	{
+		int new = a[art];
+		a[art] = a[nod];
+		a[nod] = new;
+
+		art++;
+		nod--;
+	}
+}
