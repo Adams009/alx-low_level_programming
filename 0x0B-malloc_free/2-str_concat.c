b5e56df1ccab8e249b7d1486c0f96643ba76c22d
adams009 <olaadam003@gmail.com>
2023-10-04 19:16:26 -0700

    go

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,10 +15,10 @@ char *str_concat(char *s1, char *s2)
 
 	p = o = q = r = 0;
 
-	if (s1 == (NULL))
+	if (s1 == NULL)
 		s1 = "";
 
-	if (s2 == (NULL))
+	if (s2 == NULL)
 		s2 = "";
 
 	while (s1[o])
