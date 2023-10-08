40271458c477d795727dcfe4014b4b9c126d769f
adams009 <olaadam003@gmail.com>
2023-10-08 12:29:41 -0700

    go

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _realloc - reallocates a memory
+ * @ptr: the pointer
+ * @old_size: the int old
+ * @new_size: the new size
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *point;
+	unsigned int z;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
+	{
+		point = malloc(new_size);
+
+		if (point == NULL)
+			return (NULL);
+
+		return (point);
+	}
+	else
+	{
+		if (new_size == 0)
+		{
+			free(ptr);
+			return (NULL);
+		}
+	}
+
+	point = malloc(new_size);
+
+	if (point == NULL)
+		return (NULL);
+
+	for (z = 0; z < old_size && z < new_size; z++)
+	{
+		point[z] = ((char *) ptr)[z];
+	}
+
+	free(ptr);
+	return (point);
+}
