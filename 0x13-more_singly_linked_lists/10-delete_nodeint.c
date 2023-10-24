9505da9e57f30c31f58062f7792d0dfe29cf37c0
adams009 <olaadam003@gmail.com>
2023-10-24 15:00:55 -0700

    good

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -20,7 +20,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 	temp = *head;
-	for (z = 0; z < index; z++)
+	for (z = 0; z < index - 1; z++)
 	{
 		if (temp->next == NULL)
 			return (-1);
