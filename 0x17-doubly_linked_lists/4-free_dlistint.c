64da96f8f05a8c62000bae259d5a4f27f571e266
adams009 <olaadam003@gmail.com>
2023-12-07 12:51:36 -0800

    4-free_dlistint.c

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_dlistint - function frees list
+ * @head: the head
+ * Return: void
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while(head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
